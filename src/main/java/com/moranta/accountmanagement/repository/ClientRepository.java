cb53ee241e5e02af99a5fecfd55d8cf67159c171
Moranta Fall <B@z3camP>
2022-10-30 14:51:36 +0100

    create method to retrieve account and transactions data for a client

--- src/main/java/com/moranta/accountmanagement/repository/ClientRepository.java
+++ src/main/java/com/moranta/accountmanagement/repository/ClientRepository.java
@@ -2,10 +2,15 @@ package com.moranta.accountmanagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.moranta.accountmanagement.model.Client;
+import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
     Optional<Client> findByCustomerId(String customerId);
+
+    @Query("select sum(a.amount) from Account a where a.owner= ?1")
+    BigDecimal getTotalBalance(Client client);
 }
--- src/main/java/com/moranta/accountmanagement/repository/TransactionRepository.java
+++ src/main/java/com/moranta/accountmanagement/repository/TransactionRepository.java
@@ -1,7 +1,12 @@
 package com.moranta.accountmanagement.repository;
 
+import com.moranta.accountmanagement.model.Account;
 import com.moranta.accountmanagement.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findAllByReceiverIn(List<Account> accounts);
 }
